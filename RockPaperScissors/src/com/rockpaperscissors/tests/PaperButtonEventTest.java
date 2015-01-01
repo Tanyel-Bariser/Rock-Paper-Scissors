c30e6422a51735deb62d42848f79a6bdde0dd346
Tanyel Bariser <tanyel19@hotmail-com>
2015-01-01 20:42:04 +0000

    Two unit tests for PaperButtonEvent class

---
+++ RockPaperScissors/src/com/rockpaperscissors/tests/PaperButtonEventTest.java
@@ -0,0 +1,45 @@
+package com.rockpaperscissors.tests;
+
+import static org.mockito.Mockito.verify;
+import javafx.event.ActionEvent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.rockpaperscissors.controller.RPSController;
+import com.rockpaperscissors.controller.events.PaperButtonEvent;
+import com.rockpaperscissors.model.Weapon;
+import com.rockpaperscissors.model.player.HumanPlayer;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PaperButtonEventTest {
+	@Mock RPSController controller;
+	@Mock HumanPlayer player;
+	@Mock ActionEvent action;
+	PaperButtonEvent event;
+
+	@Before
+	public void setUp() {
+		event = new PaperButtonEvent(controller, player);
+		event.HANDLE.handle(action);
+	}
+
+	@After
+	public void tearDown() {
+		event = null;
+	}
+
+	@Test
+	public void handleSetsHumanPlayerWeaponPaper() {
+		verify(player).setWeapon(Weapon.PAPER);
+	}
+	
+	@Test
+	public void handleInvokesControllerPlay() {
+		verify(controller).play();
+	}
+}
